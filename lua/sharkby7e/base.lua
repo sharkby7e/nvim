e608d13028435f27af4e8c09110b49ced74d8400
sharkby7e <sgquin@gmail.com>
2023-03-23 23:31:50 -0700

    add comments to base options

--- lua/sharkby7e/base.lua
+++ lua/sharkby7e/base.lua
@@ -1,22 +1,23 @@
 -- :help option-list
 local options = {
-ai   = true, 
-ch   = 1, 
-ea   = true,
-et   = true,
-hls  = true,
-ic   = true,
-is   = true,
-nu   = true, 
-nuw  = 4,
-rnu  = true,
-smd  = false,
-sta  = true,
-sw   = 2,
-ts   = 2,
-vb   = true,
-udf  = true, 
-wrap = false,
+  ai   = true,            -- autoindent
+  cb   = "unnamedplus",   -- clipboard
+  ch   = 1,               -- cmdheight
+  ea   = true,            -- windows always equal
+  et   = true,            -- expand tab
+  hls  = true,            -- highlight search
+  ic   = true,            -- ignore case in search 
+  is   = true,            -- incremental search
+  nu   = true,            -- numbers
+  nuw  = 4,               -- number column width
+  rnu  = true,            -- relative numbers
+  smd  = false,           -- show mode 
+  sta  = true,            -- use shiftwidth when inserting tab
+  sw   = 2,               -- shift width
+  ts   = 2,               -- tab spaces  
+  vb   = true,            -- visual bell
+  udf  = true,            -- undo file 
+  wrap = false,           -- wrap
 }
 
 for k, v in pairs(options) do
