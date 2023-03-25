2068f9179fec24e6648dac130ddc48dea8b1b53e
sharkby7e <sgquin@gmail.com>
2023-03-25 00:16:23 -0700

    am i giving up for now?

--- lua/sharkby7e/plugins/nvim-test.lua
+++ lua/sharkby7e/plugins/nvim-test.lua
@@ -1,15 +1,7 @@
 return {
   "klen/nvim-test",
+  dependencies = {'nvim-treesitter/nvim-treesitter'},
   config = function()
-    require('nvim-test').setup{
-      termOpts = {
-        direction = "vertical",   -- terminal's direction ("horizontal"|"vertical"|"float")
-        width = 96,               -- terminal's width (for vertical|float)
-        height = 15,              -- terminal's height (for horizontal|float)
-        go_back = true,          -- return focus to original window after executing
-        stopinsert = "auto",      -- exit from insert mode (true|false|"auto")
-        keep_one = true,          -- keep only one terminal for testing
-      },
-    }
-  end
+    require("nvim-test").setup({})
+  end,
 }
--- lua/sharkby7e/plugins/treesitter.lua
+++ lua/sharkby7e/plugins/treesitter.lua
@@ -1,5 +1,7 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  priority = 999,
   config = function()
     require'nvim-treesitter.configs'.setup {}
   end
