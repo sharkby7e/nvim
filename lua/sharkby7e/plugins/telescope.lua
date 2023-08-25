f3740ca1c60242534df705aa4bd5fd9035913581
sharkby7e <sgquin@gmail.com>
2023-08-24 23:47:21 -0700

    this was so easy to setup why have i been waiting

---
+++ lua/sharkby7e/plugins/telescope.lua
@@ -0,0 +1,10 @@
+return {
+  'nvim-telescope/telescope.nvim', tag = '0.1.2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+
+  vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {}),
+  vim.api.nvim_set_keymap('n', '<leader>fw', '<cmd>Telescope live_grep<cr>', {}),
+}
+
+
+
