c4510c5d8c626bd3461c2476e6761849e3ed5999
sharkby7e <sgquin@gmail.com>
2023-11-12 19:58:36 -0500

    autotag working on eruby

--- lua/sharkby7e/plugins/nvim-treesitter.lua
+++ lua/sharkby7e/plugins/nvim-treesitter.lua
@@ -6,16 +6,20 @@ return {
   cmd = { "TSUpdateSync" },
   config = function()
     require 'nvim-treesitter.configs'.setup {
+      ensure_installed = { "elixir", "heex", "eex", "ruby" },
       autotag = {
         enable = true,
+        filetypes = { "html", "xml", "eruby", "embedded_template" },
       },
       endwise = {
         enable = true,
       },
-      ensure_installed = { "elixir", "heex", "eex", "ruby" },
       highlight = {
         enable = true,
       },
+      rainbow = {
+        enable = true,
+      }
     }
   end
 }
