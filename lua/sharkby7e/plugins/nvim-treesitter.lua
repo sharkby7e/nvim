3ca77e146fbe11a16fc8ff8a5925227e39a70f14
sharkby7e <sgquin@gmail.com>
2024-03-27 08:24:20 -0400

    new colorscheme

--- lua/sharkby7e/plugins/nvim-treesitter.lua
+++ lua/sharkby7e/plugins/nvim-treesitter.lua
@@ -14,7 +14,7 @@ return {
       ensure_installed = { "vim", "vimdoc", "lua", "elixir", "heex", "eex", "ruby" },
       autotag = {
         enable = true,
-        filetypes = {"astro", "html", "xml", "typescriptreact", "eruby", "heex", "elixir", "embedded_template" },
+        filetypes = { "astro", "html", "xml", "typescriptreact", "eruby", "heex", "elixir", "embedded_template" },
       },
       highlight = {
         enable = true,
