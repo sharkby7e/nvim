714ebb57e2a8b368bde3a47ea9144b5bd10691ff
sharkby7e <sgquin@gmail.com>
2024-06-28 10:49:19 -0400

    slight adjustments to keymap

--- lua/sharkby7e/keymap.lua
+++ lua/sharkby7e/keymap.lua
@@ -17,17 +17,22 @@ map("n", "<C-down>", "<c-w><down>", opts)
 map("n", "<C-left>", "<c-w><left>", opts)
 map("n", "<C-right>", "<c-w><right>", opts)
 
+map("n", "===", "gg=G", opts)
+
 map("n", "n", "nzzzv", opts)
 map("n", "m", "Nzzzv", opts)
+
+map("i", "<c-<>", "<% %><ESC>bbei", opts)
+map("i", "<c->>", "<%= %><ESC>T=a", opts)
 
 map("n", "<S-h>", ":BufferLineCycleNext<CR>", opts)
 map("n", "<S-l>", ":BufferLineCyclePrev<CR>", opts)
 map("n", "<leader>bb", ":bd<CR>", opts)
 
-map("n", "<leader>r", "<CMD>lua require('neotest').run.run()<CR>", opts)
-map("n", "<C-r>", "<CMD>lua require('neotest').run.run_last()<CR>", opts)
-map("n", "<leader>rf", "<CMD>lua require('neotest').run.run(vim.fn.expand('%'))<CR>", opts)
-map("n", "<leader>ts", "<CMD>lua require('neotest').summary.toggle()<CR>", opts)
+-- map("n", "<leader>r", "<CMD>lua require('neotest').run.run()<CR>", opts)
+-- map("n", "<C-r>", "<CMD>lua require('neotest').run.run_last()<CR>", opts)
+-- map("n", "<leader>rf", "<CMD>lua require('neotest').run.run(vim.fn.expand('%'))<CR>", opts)
+-- map("n", "<leader>ts", "<CMD>lua require('neotest').summary.toggle()<CR>", opts)
 
 map("", "<leader>h", ":noh<CR>", opts)
 map("", "<leader>w", ":w<CR>", opts)
