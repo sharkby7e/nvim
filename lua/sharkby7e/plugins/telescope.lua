e30ba7d296f0f29e8c67c7685cfa039532d0bd1a
sharkby7e <sgquin@gmail.com>
2024-11-12 11:39:26 -0500

    update telescope pin

--- lua/sharkby7e/plugins/telescope.lua
+++ lua/sharkby7e/plugins/telescope.lua
@@ -1,10 +1,8 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.2',
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
 
   vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {}),
   vim.api.nvim_set_keymap('n', '<leader>fw', '<cmd>Telescope live_grep<cr>', {}),
 }
-
-
-
