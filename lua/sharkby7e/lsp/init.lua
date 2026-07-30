362b050266677c99063c3b04c02eff7c34c0eefb
sid <sgquin@gmail.com>
2026-07-30 12:40:26 -0400

    enable tailwindcss language server

--- lua/sharkby7e/lsp/init.lua
+++ lua/sharkby7e/lsp/init.lua
@@ -1,7 +1,7 @@
 -- mason and mason lsp-config
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls" }
+  ensure_installed = { "lua_ls", "tailwindcss" }
 })
 
 vim.diagnostic.config {
