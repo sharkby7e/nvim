afad0d823817facf9f9e3f9ebb9650ec170026af
sid <sgquin@gmail.com>
2026-08-06 03:20:53 -0400

    fix telescope preview crash on nvim-treesitter main branch
    Telescope's bundled previewer calls the old nvim-treesitter
    parsers/configs API for highlighting, which the main branch rewrite
    removed. Disable treesitter previews so it falls back to regex syntax
    highlighting instead of erroring.

--- lua/sharkby7e/plugins/telescope.lua
+++ lua/sharkby7e/plugins/telescope.lua
@@ -3,6 +3,20 @@ return {
   tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
 
-  vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {}),
-  vim.api.nvim_set_keymap('n', '<leader>fw', '<cmd>Telescope live_grep<cr>', {}),
+  config = function()
+    -- nvim-treesitter's `main` branch dropped the old parsers/configs
+    -- modules that telescope's bundled previewer relies on for
+    -- treesitter highlighting; disable it so previews use regex
+    -- `syntax` highlighting instead of erroring.
+    require('telescope').setup({
+      defaults = {
+        preview = {
+          treesitter = false,
+        },
+      },
+    })
+
+    vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {})
+    vim.api.nvim_set_keymap('n', '<leader>fw', '<cmd>Telescope live_grep<cr>', {})
+  end,
 }
